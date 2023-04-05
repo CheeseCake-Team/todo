80ae18e1539c51c97d9c7690060636842b9cb422
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-05 03:03:51 +0200

    implement BaseFragment

---
+++ app/src/main/java/com/cheesecake/todo/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.cheesecake.todo.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+
+    protected abstract val bindingInflater: (LayoutInflater) -> VB
+
+    private var _binding: VB? = null
+
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = bindingInflater(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
