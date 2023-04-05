13e8ce956ef237cc805ff904963922e92526ef29
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-05 02:46:04 +0200

    implement Base Activity class

---
+++ app/src/main/java/com/cheesecake/todo/base/BaseActivity.kt
@@ -0,0 +1,30 @@
+package com.cheesecake.todo.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+
+    protected abstract val bindingInflater: (LayoutInflater) -> VB
+
+    private var _binding: VB? = null
+
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
