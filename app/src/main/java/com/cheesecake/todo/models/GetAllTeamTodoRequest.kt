70499db2485c27f6643821df7010c18a3e3cedce
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-07 18:44:44 +0200

    adjust models class

--- app/src/main/java/com/cheesecake/todo/models/GetAllTeamTodoRequest.kt
+++ app/src/main/java/com/cheesecake/todo/models/GetAllTeamTodoRequest.kt
@@ -1,10 +1,6 @@
 package com.cheesecake.todo.models
 
 data class GetAllTeamTodoRequest(
-    val id: String,
-    val title: String,
-    val description: String,
-    val assignee: String,
-    val status: Int,
-    val creationTime: Long,
+    val allTodoList: List<TeamTodoItem>,
+    val isSuccess: Boolean,
 )
