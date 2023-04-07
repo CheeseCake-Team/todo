0394394a4e94352c44b0655b9664b2026508d18c
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-08 00:10:11 +0200

    document MyHttpClient class

---
+++ app/src/main/java/com/cheesecake/todo/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.cheesecake.todo.utils
+
+object Constants {
+    const val BASE_URL = "https://team-todo-62dmq.ondigitalocean.app"
+    const val LOGIN = "/login"
+    const val SIGNUP = "/signup"
+    const val PERSONAL = "/todo/personal"
+    const val TEAM = "/todo/team"
+
+}
