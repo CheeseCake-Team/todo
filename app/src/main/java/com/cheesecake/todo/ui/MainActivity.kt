5f78e5692aa1779d2e6450a3ebe9c3e7259566e0
Ziad Mahmoud <ziaadaboelela@gmail.com>
2023-04-08 02:34:09 +0200

    add bottomnavbar and fragment container implement sign up screen add drawables

--- app/src/main/java/com/cheesecake/todo/ui/MainActivity.kt
+++ app/src/main/java/com/cheesecake/todo/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cheesecake.todo.ui
 
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import com.cheesecake.todo.ui.base.BaseActivity
 import com.cheesecake.todo.databinding.ActivityMainBinding
@@ -9,7 +10,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding =
         ActivityMainBinding::inflate
 
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 
 }
