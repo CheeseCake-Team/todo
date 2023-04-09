b73524021d6bf08d5352d100c24952a369901ceb
Anwaar Raafat <raafatanwaar@gmail.com>
2023-04-10 00:53:30 +0200

    edit todosearch class

---
+++ app/src/main/java/com/cheesecake/todo/utils/TodoSearch.kt
@@ -0,0 +1,23 @@
+package com.cheesecake.todo.utils
+
+import com.cheesecake.todo.data.models.TodoItem
+
+class TodoSearch {
+
+
+
+
+    class TodoSearch(private val todoItem : List<TodoItem>){
+
+
+        private var searchedtodos = todoItem
+
+        fun searchByName(name: String) = TodoSearch(searchedtodos).apply {
+            searchedtodos =
+                searchedtodos.filter{ it.title.contains(name) }
+        }
+
+        fun getSearchedTodos(): List<TodoItem> = searchedtodos
+    }
+
+}
