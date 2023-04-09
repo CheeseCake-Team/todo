4c4c8cbfd7c375f2a569d645be8c099605c7af87
Ziad Mahmoud <ziaadaboelela@gmail.com>
2023-04-09 22:34:57 +0200

    create function for load fragment , create initialize show sign up screen

--- app/src/main/java/com/cheesecake/todo/ui/MainActivity.kt
+++ app/src/main/java/com/cheesecake/todo/ui/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.cheesecake.todo.ui
 
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import com.cheesecake.todo.R
 import com.cheesecake.todo.ui.base.BaseActivity
 import com.cheesecake.todo.databinding.ActivityMainBinding
+import com.cheesecake.todo.ui.base.BaseFragment
+import com.cheesecake.todo.ui.sign_up.SignUpFragment
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -11,6 +16,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         ActivityMainBinding::inflate
 
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        initializeHomeScreen()
+    }
+
+    private fun loadFragmentIntoContainer(baseFragment: BaseFragment<*>) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_activity, baseFragment).commit()
+    }
+
+    fun initializeHomeScreen() {
+        binding.bottomNavigationBar.visibility = View.GONE
+        loadFragmentIntoContainer(SignUpFragment())
+
+    }
 }
