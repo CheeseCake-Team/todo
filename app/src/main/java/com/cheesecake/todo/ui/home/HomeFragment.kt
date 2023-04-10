f8f86edbb77361c35a1708230ddb83c88d7b1763
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-11 00:24:11 +0200

    update onViewCreated method update loadViewAllFragment

--- app/src/main/java/com/cheesecake/todo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cheesecake/todo/ui/home/HomeFragment.kt
@@ -1,8 +1,11 @@
 package com.cheesecake.todo.ui.home
 
+import ViewAllTodoItemsFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import com.cheesecake.todo.R
+import com.cheesecake.todo.data.models.Tag
 import com.cheesecake.todo.databinding.FragmentHomeBinding
 import com.cheesecake.todo.data.models.TodoItem
 import com.cheesecake.todo.data.models.TodoState
@@ -13,31 +16,45 @@ class HomeFragment() : BaseFragment<FragmentHomeBinding>() {
     override val bindingInflater: (LayoutInflater) -> FragmentHomeBinding =
         FragmentHomeBinding::inflate
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val todosList =
-//            listOf<Any>(
-//                TodoItem(",", "", "", "", TodoState.TODO, ""),
-//                TodoItem(",", "", "", "",  TodoState.TODO, ""),
-//                listOf<TodoItem>(
-//                    TodoItem(",", "", "", "",  TodoState.TODO, ""),
-//                    TodoItem(",", "", "", "",  TodoState.TODO, "")
-//                )
-//            )
-        //binding.recyclerViewHome.adapter = HomeAdapter(todosList,::loadViewAllFragment)
+        val todosList =
+            listOf<TodoItem>(
+                TodoItem(",", "", "", "", TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, "")
+            )
+        val todosList0 =
+            listOf<TodoItem>(
+                TodoItem(",", "", "", "", TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, ""),
+                TodoItem(",", "", "", "",  TodoState.TODO, "")
+
+            )
+        val list = listOf(
+            DataItem.Header(10,10,20),
+            DataItem.TagItem(Tag(0,"afdsafds", todosList)),
+            DataItem.TagItem(Tag(1,"sadfsdafffff", todosList0))
+        )
+         val homeAdapter = HomeAdapter(
+            object : TodoItemClickListener {
+                override fun onTodoItemClick(todoItem: TodoItem) {
+
+                }
+            })
+        homeAdapter.submitList(list)
+        binding.recyclerViewHome.adapter = homeAdapter
     }
 
-    override fun onRetryClick() {
-        TODO("Not yet implemented")
-    }
 
-//    private fun loadViewAllFragment() {
-//        requireActivity().supportFragmentManager.beginTransaction().apply {
-//            replace(R.id.fragment_container, ViewAllFragment())
-//            addToBackStack(null)
-//            commit()
-//        }
-//    }
+    private fun loadViewAllFragment() {
+        requireActivity().supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_activity, ViewAllTodoItemsFragment())
+            addToBackStack(null)
+            commit()
+        }
+    }
 
 }
