d1c8558cb8cda80cd5e9e58c5d6381c1a8c84b02
Mohamed Emad <asd55asd36@gmail.com>
2023-04-10 14:29:06 +0200

    finish the mission

--- app/src/main/java/com/cheesecake/todo/ui/TaskDetailsFragment.kt
+++ app/src/main/java/com/cheesecake/todo/ui/TaskDetailsFragment.kt
@@ -5,24 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.cheesecake.todo.R
+import com.cheesecake.todo.databinding.FragmentTaskDetailsBinding
 import com.cheesecake.todo.ui.base.BaseFragment
 
-class TaskDetailsFragment : BaseFragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+class TaskDetailsFragment : BaseFragment<FragmentTaskDetailsBinding>() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override val bindingInflater: (LayoutInflater) -> FragmentTaskDetailsBinding
+        get() = TODO("Not yet implemented")
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_task_details, container, false)
-    }
+
+
 }
