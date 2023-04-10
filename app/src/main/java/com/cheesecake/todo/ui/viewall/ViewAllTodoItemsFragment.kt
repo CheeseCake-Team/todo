0980bbdf6fa559132046e766eed1f6bb206e2aef
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-11 00:26:15 +0200

    add string add fixed card foreground update view all fragment

--- app/src/main/java/com/cheesecake/todo/ui/viewall/ViewAllTodoItemsFragment.kt
+++ app/src/main/java/com/cheesecake/todo/ui/viewall/ViewAllTodoItemsFragment.kt
@@ -1,5 +1,3 @@
-package com.cheesecake.todo.ui.viewall
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,16 +5,28 @@ import com.cheesecake.todo.data.models.TodoItem
 import com.cheesecake.todo.data.models.TodoState
 import com.cheesecake.todo.databinding.FragmentViewAllTodoItemsBinding
 import com.cheesecake.todo.ui.base.BaseFragment
-import com.cheesecake.todo.ui.home.SearchTodosAdapter
 
 class ViewAllTodoItemsFragment : BaseFragment<FragmentViewAllTodoItemsBinding>() {
 
     override val bindingInflater: (LayoutInflater) -> FragmentViewAllTodoItemsBinding =
         FragmentViewAllTodoItemsBinding::inflate
 
+    val todosList =
+        listOf(
+            TodoItem(",rfwea", "far", "arf", "afr", TodoState.TODO, "far"),
+            TodoItem(",rfwea", "far", "arf", "afr", TodoState.TODO, "far"),
+            listOf<TodoItem>(
+                TodoItem(",rfwea", "far", "arf", null, TodoState.TODO, "far"),
+                TodoItem(",rfwea", "far", "arf", null, TodoState.TODO, "far"),
+            ), listOf<TodoItem>(
+                TodoItem(",rfwea", "far", "arf", "afr", TodoState.TODO, "far"),
+                TodoItem(",rfwea", "far", "arf", "afr", TodoState.TODO, "far"),
+            )
+        )
 
-
-
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //binding.recyclerViewAllTodos.adapter = SearchTodosAdapter(todosList[2] as List<TodoItem>)
+    }
 
 }
