a41ae4881b799dc890789dcf5a87252431b8e5a0
hassanwasfy <wasfyelgamed@gmail.com>
2023-04-15 01:34:17 +0200

    add todo hints

--- app/src/main/java/com/cheesecake/todo/data/repository/identity/AuthRepository.kt
+++ app/src/main/java/com/cheesecake/todo/data/repository/identity/AuthRepository.kt
@@ -2,6 +2,8 @@ package com.cheesecake.todo.data.repository.identity
 
 interface AuthRepository {
     fun login(username: String, password: String, loginCallback: LoginCallback)
+    // TODO
+    // convert into class
     fun signUp(username: String, password: String, teamId: String, signUpCallback: SignUpCallback)
 
     fun saveTokenAndExpireDate(token: String, expireDate: String)
