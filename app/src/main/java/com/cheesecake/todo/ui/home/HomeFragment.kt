0654ba547fe232936cf4f7bad36fa6bb4e2d9e7e
Rehammashal7 <rehammashal7@gmail.com>
2023-04-16 00:42:03 +0200

    change search fragment

--- app/src/main/java/com/cheesecake/todo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cheesecake/todo/ui/home/HomeFragment.kt
@@ -14,12 +14,8 @@ import com.cheesecake.todo.data.models.TodoState
 import com.cheesecake.todo.ui.base.BaseFragment
 
 
-class HomeFragment() : BaseFragment<FragmentHomeBinding>(), SearchView.OnQueryTextListener {
-    private var searchAdapter = TodoItemAdapter(object : TodoItemClickListener {
-        override fun onTodoItemClick(todoItem: TodoItem) {
+class HomeFragment() : BaseFragment<FragmentHomeBinding>() {
 
-        }
-    })
     private val homeAdapter = HomeAdapter(object : TodoItemClickListener {
         override fun onTodoItemClick(todoItem: TodoItem) {
 
@@ -33,44 +29,14 @@ class HomeFragment() : BaseFragment<FragmentHomeBinding>(), SearchView.OnQueryTe
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUp()
-        addCallbacks()
+
 
 
     }
 
     private fun setUp() {
         homeAdapter.submitList(list)
-        addSearchListener()
-    }
 
-    private fun addSearchListener() {
-        binding.searchBar.setOnQueryTextListener(this)
-    }
-
-    private fun searchByQueryAndSetDataInAdapter(query: String?) {
-        query?.let {
-            binding.apply {
-                setDataOnAdapter(it)
-            }
-        }
-    }
-
-    private fun setDataOnAdapter(query: String) {
-        if (query.isNotEmpty()) {
-            val resultOfSearch = searchByTitleOrDescription(query)
-            searchAdapter.submitList(resultOfSearch)
-            binding.recyclerViewHome.adapter = searchAdapter
-        } else {
-
-            binding.recyclerViewHome.adapter = homeAdapter
-        }
-
-    }
-
-    private fun addCallbacks() {
-        binding.apply {
-            recyclerViewHome.adapter = homeAdapter
-        }
     }
 
 
@@ -83,23 +49,7 @@ class HomeFragment() : BaseFragment<FragmentHomeBinding>(), SearchView.OnQueryTe
     }
 
 
-    private fun searchByTitleOrDescription(searchWord: String): List<TodoItem> {
-        return list2.filter {
-                it.title.lowercase().contains(searchWord.lowercase()) || it.description.lowercase()
-                    .contains(searchWord.lowercase())
-            }
-    }
 
-
-    override fun onQueryTextChange(newText: String?): Boolean {
-        newText?.let { searchByQueryAndSetDataInAdapter(it) }
-        return true
-    }
-
-    override fun onQueryTextSubmit(query: String?): Boolean {
-        query?.let { searchByQueryAndSetDataInAdapter(it) }
-        return true
-    }
 
     private var list2 = listOf<TodoItem>(
         TodoItem(",", "ll", "", "", TodoState.TODO, ""),
