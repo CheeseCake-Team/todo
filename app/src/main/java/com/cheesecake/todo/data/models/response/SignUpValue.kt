03bb8923ea213886500f05d27ac1cc62e21b6ef2
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-17 23:15:37 +0200

    add SignUpResponse data class

--- app/src/main/java/com/cheesecake/todo/data/models/response/SignUpValue.kt
+++ app/src/main/java/com/cheesecake/todo/data/models/response/SignUpValue.kt
@@ -6,3 +6,9 @@ data class SignUpValue(
     val userId: String,
     val username: String
 ) : Serializable
+
+data class SignUpResponse(
+    override val value: SignUpValue?,
+    override val message: String?,
+    override val isSuccess: Boolean
+) : BaseResponse<SignUpValue>(value, message, isSuccess)
