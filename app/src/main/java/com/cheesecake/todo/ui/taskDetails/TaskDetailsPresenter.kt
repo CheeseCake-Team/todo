4ef6c705a9cd177cafd8a63ea42267bd0d7b5b6a
Mujtaba Mohammed <mojtabamohamd92@gmail.com>
2023-04-19 02:08:56 +0300

    crete task details presenter and task details view make task details fragment implement task details view

---
+++ app/src/main/java/com/cheesecake/todo/ui/taskDetails/TaskDetailsPresenter.kt
@@ -0,0 +1,26 @@
+package com.cheesecake.todo.ui.taskDetails
+
+import com.cheesecake.todo.data.models.TodoItem
+import com.cheesecake.todo.data.models.TodoState
+import com.cheesecake.todo.data.repository.todos.TodoRepository
+
+class TaskDetailsPresenter(private val todoRepository: TodoRepository) {
+    private var taskDetailsView: TaskDetailsView? = null
+
+    fun attachView(view: TaskDetailsView) {
+        taskDetailsView = view
+
+
+    }
+
+    fun detachView() {
+        taskDetailsView = null
+    }
+
+    fun updateState(todo: TodoItem, isPersonal: Boolean, newState: TodoState) {
+        if (isPersonal) {
+//            todoRepository.changePersonalTodoStatus(todo.id, newState, )
+        } else {
+//            todoRepository.changeTeamTodoStatus(todo.id, newState,)
+        }
+    }
