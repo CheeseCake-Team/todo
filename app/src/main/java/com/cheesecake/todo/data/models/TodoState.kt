ca66c4f92336fa5fd498fa3623acfe0763871ed6
Abdurrahman <abdosalahas.01128@gmail.com>
2023-04-18 07:14:44 +0200

    fix todo state parsing

--- app/src/main/java/com/cheesecake/todo/data/models/TodoState.kt
+++ app/src/main/java/com/cheesecake/todo/data/models/TodoState.kt
@@ -1,5 +1,12 @@
 package com.cheesecake.todo.data.models
 
+import com.google.gson.annotations.SerializedName
+
 enum class TodoState(val value: Int) {
-    TODO(0), IN_PROGRESS(1), DONE(2)
+    @SerializedName("0")
+    TODO(0),
+    @SerializedName("1")
+    IN_PROGRESS(1),
+    @SerializedName("2")
+    DONE(2)
 }
