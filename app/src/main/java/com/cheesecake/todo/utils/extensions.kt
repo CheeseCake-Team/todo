3b9ee32bb54b24e9afd6d3c11f962872a01066a2
shehab <sheshox12@gmail.com>
2023-04-20 13:34:49 +0200

    remove unnecessary code

--- app/src/main/java/com/cheesecake/todo/utils/extensions.kt
+++ app/src/main/java/com/cheesecake/todo/utils/extensions.kt
@@ -59,13 +59,6 @@ fun setFocusAndHint(textInputLayout: TextInputLayout, editText: EditText, hint: 
     }
 }
 
-fun View.makeGone() {
-    this.visibility = View.GONE
-}
-
-fun View.makeVisible() {
-    this.visibility = View.VISIBLE
-}
 
 
 
